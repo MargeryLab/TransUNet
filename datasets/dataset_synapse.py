95604dcd7a4ff8b7de762f1a9ea873ed50a3d9ca
MargeryLab <margery753@163.com>
2021-08-04 12:26:31 +0800

    add preprocessing

--- datasets/dataset_synapse.py
+++ datasets/dataset_synapse.py
@@ -6,6 +6,7 @@ import torch
 from scipy import ndimage
 from scipy.ndimage.interpolation import zoom
 from torch.utils.data import Dataset
+import SimpleITK as sitk
 
 
 def random_rot_flip(image, label):
@@ -73,3 +74,76 @@ class Synapse_dataset(Dataset):
             sample = self.transform(sample)
         sample['case_name'] = self.sample_list[idx].strip('\n')
         return sample
+
+
+def save_npy():
+    root_path = '/media/margery/4ABB9B07DF30B9DB/DARA-SIRRUNRUN/NEW12Month/RectalCancerDataFull'
+    raw_path = '/media/margery/4ABB9B07DF30B9DB/DARA-SIRRUNRUN/NEW12Month/RectalCancerDataFull/dcmData'
+    labels_path = '/media/margery/4ABB9B07DF30B9DB/DARA-SIRRUNRUN/NEW12Month/RectalCancerDataFull/labelData'
+    train_npy_path = './data/Synapse/train_npz'
+    test_npy_path = './data/Synapse/test_npz'
+    train_ls = open(os.path.join(root_path, 'train.txt')).readlines()
+
+    train_slice_ls = []
+    test_slice_ls = []
+
+    for ID in os.listdir(labels_path):
+        data_path = os.path.join(raw_path, ID[:-7], ID[:-7], 'HRT2')
+        reader = sitk.ImageSeriesReader()
+        dcm_path = reader.GetGDCMSeriesFileNames(data_path)
+        reader.SetFileNames(dcm_path)
+        imgs = reader.Execute()
+        imgs_arr = sitk.GetArrayFromImage(imgs)
+        imgs_arr = (imgs_arr-pixel_mean)/pixel_std
+
+        label_path = os.path.join(labels_path, ID)
+        label = sitk.ReadImage(label_path)
+        label_arr = sitk.GetArrayFromImage(label)
+        label_arr[label_arr == 4] = 1  # 黄色交界处定义为肿瘤
+        label_arr[label_arr > 2] = 0 #包含012
+
+        if ID[:-7]+'\n' in train_ls:
+            pass
+            # for i in range(1, label_arr.shape[0]+1):
+            #     if i < 10:
+            #         np.savez(os.path.join(train_npy_path, ID[:-7] +'-0{}'.format(i)+'.npz'), image=imgs_arr[i-1], label=label_arr[i-1])  #按病例保存npy
+            #         train_slice_ls.append(ID[:-7] +'-0{}'.format(i))
+            #     else:
+            #         np.savez(os.path.join(train_npy_path, ID[:-7] + '-{}'.format(i) + '.npz'), image=imgs_arr[i-1], label=label_arr[i-1])
+            #         train_slice_ls.append(ID[:-7] + '-{}'.format(i))
+        else:
+            test_slice_ls.append(ID[:-7])
+            h5f = h5py.File(os.path.join(test_npy_path, ID[:-7]+'.npy.h5'), 'w')
+            h5f.create_dataset('image', data=imgs_arr)
+            h5f.create_dataset('label', data=label_arr)
+            h5f.close()
+            # for i in range(1, label_arr.shape[0] + 1):
+            #     if i < 10:
+            #         np.savez(os.path.join(test_npy_path, ID[:-7] + '-0{}'.format(i) + '.npz'), image=imgs_arr[i - 1],
+            #                  label=label_arr[i - 1])  # 按病例保存npy
+            #         test_slice_ls.append(ID[:-7] + '-0{}'.format(i))
+            #     else:
+            #         np.savez(os.path.join(test_npy_path, ID[:-7] + '-{}'.format(i) + '.npz'), image=imgs_arr[i - 1],
+            #                  label=label_arr[i - 1])
+            #         test_slice_ls.append(ID[:-7] + '-{}'.format(i))
+
+    # with open('data/Synapse/train.txt', mode='w+') as file:
+    #     file.writelines([line+'\n' for line in train_slice_ls])
+
+    with open('data/Synapse/test_vol.txt', mode='w+') as file:
+        file.writelines([line + '\n' for line in test_slice_ls])
+
+
+if __name__ == '__main__':
+    # for i in range(imgs_arr.shape[0]):
+    #     m,s = cv2.meanStdDev(imgs_arr[i])
+    #     m_list.append(m)
+    #     s_list.append(s)
+    # m = np.mean(m_list)
+    # s = np.mean(s_list)
+    # print(m)
+    # print(s)
+
+    # m = np.mean(m_list)
+    # s = np.mean(s_list)
+    save_npy()
